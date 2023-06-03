52b0108e47342f5dc4c55ab3eea63173d700b082
Mukhutdinov Ruslan <mukhutdinov.ruslan@gmail.com>
2023-06-03 17:14:51 +0500

    add post

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
+<template>
+  <div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+  </div>
+</template>
+
 <script setup>
 useHead({
   script: [
-    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
-  ]
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+const { data: posts } = useAsyncData(async () => {
+  const posts = await $content("blog").fetch();
+  return { posts };
 });
 </script>
